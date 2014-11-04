a22552f20a42af13a6c6e94f0999df68a471878f
marvin laske <marvin.laske@hotmail.de>
2014-11-04 21:27:32 +0100

    test

--- tests/scrapi.py
+++ tests/scrapi.py
@@ -2,10 +2,16 @@ __author__ = 'Marvin Laske'
 
 import unittest
 from scrapyd_api.scrapi import ScrapydApi
+from time import sleep
 
 
 class ScrapydApiTests(unittest.TestCase):
+    def setUp(self):
+        #waiting for scrapyd to start up
+        #TODO: find a more reliable solution
+        sleep(5)
+
     def test_if_clean_install(self):
-        api = ScrapydApi("http://localhost:6800/")
+        api = ScrapydApi("http://127.0.0.1:6800/")
         projects = api.list_projects()
         print projects
